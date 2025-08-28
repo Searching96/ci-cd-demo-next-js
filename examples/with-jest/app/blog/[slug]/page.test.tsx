082139d50996a345de6deaf4c9750eb8c543e24e
Searching96 <nguyenphucthinh2k5@gmail.com>
2025-08-29 06:37:48 +0700

    fix: be correct

--- examples/with-jest/app/blog/[slug]/page.test.tsx
+++ examples/with-jest/app/blog/[slug]/page.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import Page from "./page";
 
+export const dynamic = "force-static";
+
 describe("Blog slug page", () => {
   it("renders slug correctly", () => {
     render(<Page params={{ slug: "Test" }} />);
