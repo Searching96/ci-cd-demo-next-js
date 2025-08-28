51ca99c0569cebc9de4b23c1186ef15870d66cb1
Searching96 <nguyenphucthinh2k5@gmail.com>
2025-08-29 05:49:50 +0700

    fix: resolve incompatible setup for newer next version

--- examples/with-jest/next.config.js
+++ examples/with-jest/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    outputFileTracingRoot: __dirname, // force root to this project
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+        outputFileTracingRoot: __dirname, // force root to this project
+    },
 
     typescript: {
         ignoreBuildErrors: true,
